8f4e777618779c83992a7531c0f4c0029a238b43
Arnav Deshpande <86890832+andy1924@users.noreply.github.com>
2026-07-23 17:46:51 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0 , age = 0;
+        for(int i = 0; i < details.size(); i++){
+           age = (details[i][11] - '0')*10 + (details[i][12] - '0');
+           if(age>60) count++;
+        }
+        return count;
+    }
+};
