20a66506e723bf1c8788ac040b3adcbfcbb00824
Arnav Deshpande <86890832+andy1924@users.noreply.github.com>
2026-08-04 19:58:44 +0530

    Add: next-greater-element-i - submission-0

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+
+        for (int i = 0; i < nums1.size(); i++) {
+            for (int j = 0; j < nums2.size(); j++) {
+                if (nums1[i] == nums2[j]) {
+
+                    int nextGreater = -1;
+
+                    for (int k = j + 1; k < nums2.size(); k++) {
+                        if (nums2[k] > nums2[j]) {
+                            nextGreater = nums2[k];
+                            break;
+                        }
+                    }
+
+                    ans.push_back(nextGreater);
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
